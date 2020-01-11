d5a940cf32edd6b7deccde5f0da45943003e8a5d
weitianliang <weitianliang@dailyinnovation.biz>
2020-01-11 22:58:26 +0800

    [fix]:有效括号

---
+++ src/src/Valid_Parentheses.kt
@@ -0,0 +1,22 @@
+/**
+ * 有效括号
+ */
+fun isValid(s: String): Boolean {
+    var str = s
+    while (true) {
+        val len = str.length
+        // 此时循环会生成大量 String 对象，耗时且耗费资源。
+        // 扩展 StringBuilder 方法实现字符串替换，代替 String。
+        str = str.replace("()", "")
+        str = str.replace("{}", "")
+        str = str.replace("[]", "")
+        if (len == str.length) {
+            break
+        }
+    }
+    return str.isEmpty()
+}
+
+fun main() {
+    print(isValid("[()]"))
+}
