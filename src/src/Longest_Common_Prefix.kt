9855cdd9d9a7555cbd7dbf42b4b2aba3f3c47182
weitianliang <weitianliang@dailyinnovation.biz>
2020-01-12 11:44:07 +0800

    [fix]:最长公共前缀

---
+++ src/src/Longest_Common_Prefix.kt
@@ -0,0 +1,32 @@
+import java.lang.StringBuilder
+import kotlin.math.min
+/**
+ * 最长公共前缀
+ */
+fun longestCommonPrefix(strs: Array<String>): String {
+    if (strs.isEmpty()) {
+        return ""
+    }
+    var mid = strs[0]
+    strs.forEach {
+        mid = twoWordCommonPrefix(mid, it)
+    }
+    return mid
+}
+
+fun twoWordCommonPrefix(str1: String, str2: String): String {
+    val builder = StringBuilder()
+    for (i in 0 until min(str1.length, str2.length)) {
+        if (str1[i] == str2[i]) {
+            builder.append(str1[i])
+        }
+        if (str1[i] != str2[i]) {
+            break
+        }
+    }
+    return builder.toString()
+}
+
+fun main() {
+    print(longestCommonPrefix(arrayOf("flower", "flow", "flight")))
+}
