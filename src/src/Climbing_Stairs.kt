9931914823dd76b6bd8acb2648dfaf7bdadfc8e5
weitianliang <weitianliang@dailyinnovation.biz>
2020-01-14 22:56:54 +0800

    [fix]:爬楼梯

---
+++ src/src/Climbing_Stairs.kt
@@ -0,0 +1,30 @@
+/**
+ * 爬楼梯
+ */
+fun climbStairs(n: Int): Int {
+    return when {
+        n <= 1 -> {
+            1
+        }
+        n == 2 -> {
+            2
+        }
+        else -> {
+            var res = 0
+            var i = 1
+            var j = 2
+            var k = 3
+            while (k <= n) {
+                res = i + j
+                i = j
+                j = res
+                k++
+            }
+            res
+        }
+    }
+}
+
+fun main() {
+    print(climbStairs(5))
+}
