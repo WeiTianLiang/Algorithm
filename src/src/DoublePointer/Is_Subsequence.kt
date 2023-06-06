4b8a2f3a477deb053602d90582e5f8ca3badece7
WTL <1823480210@qq.com>
2023-06-07 00:19:49 +0800

    新的算法提交

---
+++ src/src/DoublePointer/Is_Subsequence.kt
@@ -0,0 +1,12 @@
+package DoublePointer
+
+/**
+ * leetcode 392 easy
+ * 判断子序列
+ */
+fun isSubsequence(s: String, t: String): Boolean {
+    return false
+}
+fun main() {
+    print(isSubsequence("abc", "ahbgdc"))
+}
---
+++ src/src/DoublePointer/Move_Zeroes.kt
@@ -0,0 +1,34 @@
+package DoublePointer
+
+/**
+ * leetcode 283 easy
+ * 移动零
+ */
+fun moveZeroes(nums: IntArray) {
+    if (nums.isEmpty() || nums.size == 1) {
+        return
+    }
+    var index = 0
+    var j = 0
+    var zeroCount = 0
+    while (index < nums.size) {
+        if (nums[index] == 0) {
+            zeroCount++
+        } else {
+            nums[j] = nums[index]
+            j++
+        }
+        index++
+    }
+    for (i in j until nums.size) {
+        nums[i] = 0
+    }
+}
+
+fun main() {
+    val arr = intArrayOf(0, 1, 0, 3, 12)
+    moveZeroes(arr)
+    arr.forEach {
+        print("$it ")
+    }
+}
