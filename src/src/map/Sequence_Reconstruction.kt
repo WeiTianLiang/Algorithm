6c815ee30ce3a8fa21ca7f6546fff16694c3d38a
weitianliang <weitianliang@dailyinnovation.biz>
2023-06-09 12:03:26 +0800

    新的更改

--- src/src/map/Sequence_Reconstruction.kt
+++ src/src/map/Sequence_Reconstruction.kt
@@ -1,65 +1,25 @@
 package map
 
-import java.util.Stack
-import kotlin.math.max
-
 /**
  * 剑指offerII 115 medium
  * 重建序列
  */
 private fun sequenceReconstruction(nums: IntArray, sequences: Array<IntArray>): Boolean {
-    // 建图
-    val map = mutableMapOf<Int, ArrayList<Int>>()
+    val map = mutableMapOf<Int, Int>()
+    var count = 0
+    for (i in 0 until nums.size - 1) {
+        map[nums[i]] = nums[i + 1]
+        count++
+    }
     sequences.forEach {
-        var i = 0
-        while (i < it.size - 1) {
-            var list = map[it[i]]
-            if (list == null) {
-                list = arrayListOf()
-                map[it[i]] = list
+        for (i in 0 until it.size - 1) {
+            if (map[it[i]] == it[i + 1]) {
+                count--
+                map[it[i]] = -1
             }
-            list.add(nums[i + 1])
-            i++
         }
     }
-
-    // 寻找根节点, 统计各节点对应的数量
-    val rootStack = Stack<Int>()
-    val countMap = mutableMapOf<Int, Int>()
-    map.forEach(action = {
-        if (it.value.size == 0) {
-            rootStack.push(it.key)
-        } else {
-            countMap[it.key] = it.value.size
-        }
-    })
-
-    // 深度遍历有向图
-    var maxDepth = 0
-    var depth = 0
-    // 用于遍历的栈
-    val loopStack = Stack<Int>()
-    while (true) {
-        if (loopStack.isEmpty()) {
-            // 添加根节点
-            maxDepth = maxDepth.coerceAtLeast(depth)
-            depth = 0
-            if (rootStack.isEmpty()) {
-                break
-            }
-            loopStack.push(rootStack.pop())
-        }
-        val key = loopStack.pop()
-        depth++
-        map.forEach(action = {
-            if (key == it.key && countMap[key] != 0) {
-                loopStack.push(it.value[it.value.size - countMap[key]!!])
-            }
-        })
-    }
-    print("maxDepth : $maxDepth    ")
-
-    return maxDepth == nums.size
+    return count == 0
 }
 
 fun main() {
